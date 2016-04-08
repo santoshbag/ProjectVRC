3b8cb22506999e0e0c99f53295c919ce9a75636b
santoshbag <santosh.bag@gmail.com>
2016-04-08 12:33:19 -0400

    Update portfolio.py

--- instruments/portfolio.py
+++ instruments/portfolio.py
@@ -15,47 +15,33 @@ class Portfolio:
         self.portfolio_name = name
         self.position_date = datetime.today
         self.portfolio_currency = currency
-        self.portfolio_list = []
+        self.position_list = []
         
     def getPortfolioDetails(self):
         portfolioDetails = {'portfolio_name': self.portfolio_name, 
         'position_date': self.position_date, 
         'portfolio_currency': self.portfolio_currency, 
-        'portfolio_list': self.portfolio_list}
+        'position_list': self.position_list}
         
         return portfolioDetails
         
-    def add(self,singleportfolio):
-        self.portfolio_list.append(singleportfolio)
+    def addPosition(self,position_list):
+        for position in position_list:
+            self.portfolio_list.append(position)
         #return self.portfolio_list.count()
 
-class SinglePortfolio(Portfolio):
-    def setPortfolio(self,setupparams):
+    
+class Position():
+    def __init__(self,setupparams):
         self.product_type = setupparams['product_type']
         self.product_quantity = setupparams['product_quantity']
         self.product_price = setupparams['product_price']
         self.product_cost = setupparams['product_cost']
+        self.product_currency = setupparams['product_currency']
 
 
 if __name__ == '__main__':
-    CS = SinglePortfolio('Credit Suisse CDS')
-    DB = SinglePortfolio('Deutsche Bank FI','EUR')
-    GS = SinglePortfolio('Goldman MBS')
-    
-    
-    
-    master = Portfolio('CS')
-    master.add(CS)
-    master.add(DB)
-    master.add(GS)
-    
-    #print(CS.portfolio_name,CS.portfolio_currency)
-    #det = {}    
-    #det = CS.getPortfolioDetails()
-    
-    print(master.getPortfolioDetails())
-    for plist in master.getPortfolioDetails()['portfolio_list']:
-        print(plist.getPortfolioDetails())
+   
     
 
         

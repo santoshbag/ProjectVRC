3188d74dc0d2fd0d7544f722fd12edffc3f53f8a
santoshbag <santosh.bag@gmail.com>
2016-04-08 12:34:01 -0400

    Update bonds.py

--- instruments/bonds.py
+++ instruments/bonds.py
@@ -6,5 +6,14 @@ Created on Fri Apr  8 11:20:48 2016
 """
 
 class Bond:
-    pass
+    def __init__(self,name=Bond1):
+        self.issue_name = name
+        self.security_type = "Bond"
+        
+    def setupBond(self,setupparams):
+        self.issue_date = setupparams['issue_date']
+        self.maturity_date = setupparams['maturity_date']
+        self.coupon_frequency = setupparams['coupon_frequency']
+        self.day_count = setupparams['day_count']
+        
 
